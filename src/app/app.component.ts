1094dc4eee28048448198d2806a4e82b494bc4be
VakeDomen <vake.domen94@gmail.com>
2024-04-30 14:35:33 +0200

    empty angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'llm_usrka_web_client';
+}
